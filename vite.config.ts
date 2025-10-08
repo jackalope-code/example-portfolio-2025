9391dd20706683266cac0b1e61fd1ca59dbc910a
jackalope-code <22228272+jackalope-code@users.noreply.github.com>
2025-10-08 09:52:42 -0600

    Remove base URL argument to default to / for root dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/jackalope-code/example-portfolio-2025.git",
   build: {
     outDir: 'build',
   }
