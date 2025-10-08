0109b93a7bb937970d3c802c4e2d7f31a6e02f61
jackalope-code <22228272+jackalope-code@users.noreply.github.com>
2025-10-08 10:18:29 -0600

    Base URL is perfect now

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'example-portfolio-2025/',
+  base: '/example-portfolio-2025/',
   plugins: [react()],
   build: {
     outDir: 'build',
